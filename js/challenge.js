fa65acfe156a5f6c664f5fd1aefe856b8db1ea54
lycho33 <cho.lyds@gmail.com>
2021-06-27 13:16:11 -0400

    commit tree 48250685ed64573af4a0881a7578c09773bcdd84

---
+++ js/challenge.js
@@ -0,0 +1,11 @@
+function timer(timeP) {
+    var d = new Date();
+    var sec = d.getSeconds();
+    lblJsTimer.SetText(sec);
+}
+
+function timer(sec) {
+    let c = document.getElementById("counter")
+    
+    document.write(sec)
+}
